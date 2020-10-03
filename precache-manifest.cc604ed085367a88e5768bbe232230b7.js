463053b83126085ab91c17b1f2bf9a529578752e
DzabievaAA <DzabievaAA@gmail.com>
2020-10-03 17:37:56 +0300

    Updates

---
+++ precache-manifest.cc604ed085367a88e5768bbe232230b7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9719a41db18b1c0fe332bfe5ee1d2483",
+    "url": "/TodoList/index.html"
+  },
+  {
+    "revision": "3ad493364407cb416ac6",
+    "url": "/TodoList/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ba41752fc1ebf2a4ed4a",
+    "url": "/TodoList/static/js/2.162d09dc.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/TodoList/static/js/2.162d09dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ad493364407cb416ac6",
+    "url": "/TodoList/static/js/main.9aec1df0.chunk.js"
+  },
+  {
+    "revision": "894fafee523fe812d833",
+    "url": "/TodoList/static/js/runtime-main.c8474f3e.js"
+  }
+]);
